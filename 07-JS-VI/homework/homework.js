c0fadf64248ce09ef2f1b381264a83efeb71c62b
brendalozano3108 <blozano651@gmail.comn>
2022-10-08 07:54:56 -0500

    prueba 1

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,20 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  return nombre[0].toUpperCase() + nombre.slice(1);
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  return cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1, n2, cb);
 }
 
 function sumarArray(numeros, cb) {
@@ -22,6 +25,10 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  var sumaenteros = numeros.reduce(function (c1, c2) {
+    return c1 + c2;
+  }, 0);
+  return cb(sumaenteros);
 }
 
 function forEach(array, cb) {
@@ -53,5 +60,5 @@ module.exports = {
   sumarArray,
   forEach,
   map,
-  filter
+  filter,
 };
